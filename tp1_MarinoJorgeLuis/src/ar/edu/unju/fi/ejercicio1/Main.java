324e40edfc411686c2442fa13f854ddff30cfcec
marjo <marjo@DESKTOP-RMIAIB0>
2024-04-09 10:16:13 -0300

    Agrego ejercicio 1 y 2 resueltos.

---
+++ tp1_MarinoJorgeLuis/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,17 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=8;
+		int b=5;
+		int c=1;
+		int d=20;
+		int e=9;
+		float suma=a+b+c+d+e;
+		float res=suma/5;
+		System.out.println(res);
+	}
+
+}
---
+++ tp1_MarinoJorgeLuis/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,22 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String pais="España";
+		Short edad=26;
+		Short altura=20;
+		Double precio=6.8;
+		String telefono="+54 41 1234-5678";
+		Double coseno=0.5;
+		
+		System.out.println("Nombre de un país: "+pais);
+		System.out.println("Edad de una persona: "+edad);
+		System.out.println("Altura de un edificio: "+altura);
+		System.out.println("Precio de producto: $"+precio);
+		System.out.println("Número de teléfono: "+telefono);
+		System.out.println("Coseno de 0.5: "+Math.cos(coseno));
+	}
+
+}
